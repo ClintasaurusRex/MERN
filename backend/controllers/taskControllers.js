809764d5752df58aaec89f4714b265233fcf3072
Clint <clinthiles1992@gmail.com>
2025-02-06 13:05:29 -0800

    first commit

---
+++ backend/controllers/taskControllers.js
@@ -0,0 +1,22 @@
+const getTasks = (req, res) => {
+  res.status(200).json({ message: "Get all tasks" });
+};
+
+const setTask = (req, res) => {
+  if (!req.body.text) {
+    res.status(400).json({ message: "Please enter a task" });
+    return;
+  }
+
+  res.status(201).json({ message: "Create a task" });
+};
+
+const updateTask = (req, res) => {
+  res.status(200).json({ message: `Update task ${req.params.id}` });
+};
+
+const deleteTask = (req, res) => {
+  res.status(200).json({ message: `Delete task ${req.params.id}` });
+};
+
+module.exports = { getTasks, setTask, updateTask, deleteTask };
