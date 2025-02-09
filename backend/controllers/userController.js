db23cc75504b1fee3a04eed4942b8cc647f8cca4
Clint <clinthiles1992@gmail.com>
2025-02-09 12:48:24 -0800

    Refactor userController.js and userRoutes.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,20 +4,58 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 const registerUser = asyncHandler(async (req, res) => {
+  // Get data from request body
   const { name, email, password } = req.body;
+  // Validation
   if (!name || !email || !password) {
     res.status(400);
     throw new Error("Please fill in all fields");
   }
+  // Check if user exists
   const userExists = await User.findOne({ email });
   if (userExists) {
     res.status(400);
     throw new Error("User already exists");
   }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+  // after starting this route use code below to test it
   // res.json({ message: "Register User: Successful" });
 });
 
 const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials");
+  }
+
   // res.json({ message: "Login User: Successful" });
 });
 
